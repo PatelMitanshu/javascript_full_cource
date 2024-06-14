947ba12459b01b62f60f0279eb31befd88462b85
Patel Mitanshu <136210556+mitanshu123@users.noreply.github.com>
2024-06-14 23:00:54 +0530

    Update Data_and_Time.js

--- Data_and_Time.js
+++ Data_and_Time.js
@@ -2,3 +2,6 @@ let myDate = new Data()
 console.log(myDate.toString())
 console.log(myDate.toDateString())
 console.log(myDate.ToLocalString())
+
+
+// Thank you to Hitesh Choudhary sir for teaching us
